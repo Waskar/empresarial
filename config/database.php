96a631f287f626dd2b3ff0cc81043974a8a87538
Waskar <waskar007@gmail.com>
2018-06-03 22:10:53 -0400

    configurando la BD

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_bc38adb42d42d3b'),
+            'username' => env('DB_USERNAME', 'bcf47d8dc846d0'),
+            'password' => env('DB_PASSWORD', '17fe4f7d'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
